3ef206b3f864820638bc47a2aab54d316f64ea3c
LanYi <ruby2010ruby@163.com>
2020-02-11 10:41:48 +0800

    init with travis-ci

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -e $HOME/disted/ ]
+then
+    rm -rf $HOME/disted/
+fi
+mkdir $HOME/disted
+#wget https://dl.google.com/go/go1.9.2.windows-amd64.msi -P $HOME/disted/
+#wget https://dl.google.com/go/go1.9.2.src.tar.gz  -P $HOME/disted/
+#wget https://dl.google.com/go/go1.9.2.linux-amd64.tar.gz -P $HOME/disted/
+#wget https://www.google.com/chrome/browser/thankyou.html?standalone=1&platform=win&installdataindex=defaultbrowser -P $HOME/disted/
+ls -alh $HOME/disted/
+ls -lh $HOME/disted/*
+echo Hello
