e3e2dbccb3b86fe305a99462dd9cb2766246ed88
LanYi <ruby2010ruby@163.com>
2020-02-20 10:12:59 +0800

    fix error

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,5 @@
-mod utils;
 
-use wasm_bindgen::prelude::*;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub fn main() {
+    println!("Hello, wasm-game-of-life!");
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
-}
-
-#[wasm_bindgen]
-pub fn greet1(name: &str) {
-    let greets: String = format!("Hello ,{} ", name);
-    alert(&greets);
-}
-
-#[wasm_bindgen]
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Cell {
-    Dead = 0,
-    Alive = 1,
-}
-
-#[wasm_bindgen]
-pub struct Universe {
-    width: u32,
-    height: u32,
-    cells: Vec<Cell>,
-}
